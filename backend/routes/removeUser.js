abe4ee83baa54e3303b00ba878f4a761007fcc7e
Fsalker <andreiii500@yahoo.com>
2019-01-15 14:51:10 +0200

    Fixed /removeUser

--- backend/routes/removeUser.js
+++ backend/routes/removeUser.js
@@ -8,10 +8,10 @@ router.post("/" + apiName, async(req, res) => {
     let {username, password} = req.body
     password = hash(password)
 
-    if((await client.query("SELECT id FROM users WHERE LOWER(username) = LOWER($1)", [username])).rows.length == 0)
+    if((await client.query("SELECT username FROM users WHERE LOWER(username) = LOWER($1)", [username])).rows.length == 0)
       return res.status(404).end()
 
-    if((await client.query("SELECT id FROM users WHERE LOWER(username) = LOWER($1) AND password = $2", [username, password])).rows.length == 0)
+    if((await client.query("SELECT username FROM users WHERE LOWER(username) = LOWER($1) AND password = $2", [username, password])).rows.length == 0)
       return res.status(401).end()
 
     let userId = (await client.query("SELECT id FROM users WHERE LOWER(username)=LOWER($1)", [username])).rows[0].id
